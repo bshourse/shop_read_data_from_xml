43f3d499e48ed88d6d385efead145cc85eb27520
Dmitry <agapov.box@gmail.com>
2021-01-15 12:25:12 +0300

    Ability to add new product to xml file

--- book.rb
+++ book.rb
@@ -9,4 +9,23 @@ class Book < Product
     "Книга: #{@title}, автор: #{@author}"
   end
 
+# Переопределим метод чтения книги из консоли
+  def read_from_console
+    puts "Укажите название книги"
+    @title = STDIN.gets.chomp
+
+    puts "Укажите автора книги"
+    @author = STDIN.gets.chomp
+  end
+#Переопределим метод to_xml
+
+  def to_xml
+    product = super
+    product.add_element('book', {
+        'title' => @title,
+        'author_name' => @author
+    }) # в тег product, который мы берем из родителя добавляем тег book и его атрибуты
+    return product
+  end
+
 end
--- movie.rb
+++ movie.rb
@@ -10,4 +10,26 @@ class Movie < Product
     "Фильм #{@title}, режиссер - #{@director_name}, год - #{@year}"
   end
 
+  # Переопределим метод чтения фильма из консоли
+  def read_from_console
+    puts "Введите название фильма"
+    @title = STDIN.gets.chomp
+
+    puts "Введите ФИО режисера"
+    @director_name = STDIN.gets.chomp
+
+    puts "Введите год создания фильма"
+    @year = STDIN.gets.chomp
+  end
+
+  #Переопределим метод to_xml
+  def to_xml
+    product = super
+    product.add_element('movie', {
+        'title' => @title,
+        'director_name' => @director_name,
+        'year' => @year
+    }) #добавляем тег movie и его атрибуты
+    return product
+  end
 end
